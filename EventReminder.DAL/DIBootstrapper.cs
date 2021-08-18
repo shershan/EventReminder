10ec6e7e148ecd942ddf7ab5a6f8cf202d742f4d
Andrei Shershan <dev.shershan@gmail.com>
2021-08-18 18:52:26 +0300

    Add DAL DI Bootstrapper

---
+++ EventReminder.DAL/DIBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using EventBuilder.Constants;
+using EventReminder.Models.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventReminder.DAL
+{
+    public static class DIBootstrapper
+    {
+        public static IServiceCollection InitDal(this IServiceCollection services, IConfiguration configuration)
+        {
+            var settings = new DbSettings();
+            configuration.GetSection(ConfigurationConstants.DBSectionName).Bind(settings);
+
+            services.AddDbContextPool<DbContext, EventNotificationDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    settings.DefaultConnection,
+                    b => b.MigrationsAssembly(settings.MigrationAsseblyName))
+                    .ConfigureWarnings(builder =>
+                    {
+                        builder.Default(WarningBehavior.Ignore);
+                    })
+                    .EnableDetailedErrors()
+                    .EnableSensitiveDataLogging();
+            }, settings.DbPoolSize);
+
+            return services;
+        }
+    }
+}
