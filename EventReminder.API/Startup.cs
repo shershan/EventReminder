83238844cd8735ce2f0211a6dde34ef87f47d698
Andrei Shershan <dev.shershan@gmail.com>
2021-08-18 19:04:53 +0300

    Add Db Initizlization to Startup

--- EventReminder.API/Startup.cs
+++ EventReminder.API/Startup.cs
@@ -1,21 +1,30 @@
+using Arch.EntityFrameworkCore.UnitOfWork;
+using EventReminder.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EventReminder.API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConfiguration>(_configuration);
+
+            this.InitDI(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +44,17 @@ namespace EventReminder.API
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+        }
+
+        private IServiceCollection InitDI(IServiceCollection services)
+        {
+            services.InitDal(_configuration);
+
+            services
+                .AddDbContext<EventNotificationDbContext>()
+                .AddUnitOfWork<EventNotificationDbContext>();
+
+            return services;
         }
     }
 }
